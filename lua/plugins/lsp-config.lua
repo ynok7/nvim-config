6575189f90401f5c21af040e9ac8ee036dff5082
ynok7 <gabrielyuu727@gmail.com>
2024-03-15 13:58:58 -0300

    add plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,25 @@
 return {
-	"williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-	end,
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			-- Connect communication between nvim and mason-lsp server
+			lspconfig.lua_ls.setup({})
+		end,
+	},
 }
